893fdff66996ee64019316a432c89e401970b651
dnjswns619 <dnjswns619@naver.com>
2022-10-20 14:14:48 +0900

    add comment, modify scss #2

--- js/main.js
+++ js/main.js
@@ -14,8 +14,8 @@
         canvas: document.querySelector('#video-canvas-0'),
         context: document.querySelector('#video-canvas-0').getContext('2d'),
         imgPath: [
-          '../images/AirPods (1).png',
-          '../images/AirPods (2).png'
+          './images/AirPods (1).png',
+          './images/AirPods (2).png'
         ],
         images: []
       },
@@ -154,6 +154,7 @@
     //각 섹션의 높이를 정하는 식
     for (let i = 0; i < sceneInfo.length; i++) {
       if (sceneInfo[i].type === 'sticky') {
+        // type이 sticky 일때만 실행
         sceneInfo[i].scrollHeight = window.innerHeight * sceneInfo[i].heightNum;
         sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
       }
@@ -172,6 +173,8 @@
     document.body.setAttribute('id', `show-scene-${currentScene}`);
 
     if(sceneInfo[currentScene].type === 'sticky') {
+      //캔버스에 그리는 이미지가 크기와 맞지 않을때 화면에 채우기 위한 식
+      // 현재 화면 크기 / canvas 설정 크기
       let widthRatio = window.innerWidth / sceneInfo[currentScene].objs.canvas.width;
       let heightRatio = window.innerHeight / sceneInfo[currentScene].objs.canvas.height;
       let canvasScaleRatio;
@@ -186,6 +189,7 @@
   }
 
   function checkMenu() {
+    //nav의 높이보다 많이 스크롤시 local-nav position 변경
     if (yOffset > 52) {
       document.querySelector('#header').classList.add('local-nav-sticky');
     } else {
@@ -196,7 +200,7 @@
   let yOffset = 0 //현재 스크롤한 값을 담음
   let prevScrollHeight = 0; //현재 스크롤 값 이전에 위치한 섹션의 높이를 담음
   let currentScene = 0; //현재 위치한 섹션의 순서를 담음
-  let enterNewScene = false;
+  let enterNewScene = false; 
   function scrollLoop() {
     //섹션을 넘어갈 떄 마다 섹션의 값을 변화 시키는 함수
     prevScrollHeight = 0;
@@ -204,27 +208,30 @@
     for (let i = 0; i < currentScene; i++) {
       prevScrollHeight += sceneInfo[i].scrollHeight;
     }
+    //스크롤 효과가 있는 섹션에 들어가면 scroll-effect-end 클래스 제거
     if (delayYOffSet < prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       document.body.classList.remove('scroll-effect-end');
     }
     if (delayYOffSet > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       enterNewScene = true;
+      //스크롤 효과가 있는 섹션이 끝나면 scroll-effect-end 클래스 추가
       if (currentScene === sceneInfo.length - 1) {
         document.body.classList.add('scroll-effect-end');
       }
       if (currentScene < sceneInfo.length - 1) {
+        //sceneInfo에 담아둔 길이까지 도달했을 때 더이상 숫자가 증가하지 안도록 조건 추가
         currentScene++
       }
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
     if (delayYOffSet < prevScrollHeight) {
-      if (currentScene === 0) return;
+      if (currentScene === 0) return; //섹션의 숫자가 0 아래로 떨어지지 않게
       enterNewScene = true;
       currentScene--;
       document.body.setAttribute('id', `show-scene-${currentScene}`);
     }
 
-    if(enterNewScene) return
+    if(enterNewScene) return; // 섹션을 넘어갈때 이상한 값이 나오는 것을 방지
 
     playAnimation();
 
@@ -232,7 +239,7 @@
 
   function calcValues(values, currentYOffset) {
     let scrollHeight = sceneInfo[currentScene].scrollHeight;
-    let scrollRatio = currentYOffset / scrollHeight;
+    let scrollRatio = currentYOffset / scrollHeight; // 현재 스크롤값의 비율
     let rv;
     if (values.length === 3) { 
       //시작 시점과 끝나는 시점을 가진 함수만 실행
@@ -258,28 +265,28 @@
     let imgElem;
     for (let i = 0; i < sceneInfo[1].values.videoImageCount; i++) {
       imgElem = new Image();
-      imgElem.src = `../images/001/AirPods (${1 + i}).jpg`;
+      imgElem.src = `./images/001/AirPods (${1 + i}).jpg`;
       sceneInfo[1].objs.videoImages.push(imgElem)
     }
 
     let imgElem2;
     for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
       imgElem2 = new Image();
-      imgElem2.src = `../images/002/AirPods (${1 + i}).jpg`;
+      imgElem2.src = `./images/002/AirPods (${1 + i}).jpg`;
       sceneInfo[2].objs.videoImages.push(imgElem2)
     }
 
     let imgElem3;
     for (let i = 0; i < sceneInfo[3].values.videoImageCount; i++) {
       imgElem3 = new Image();
-      imgElem3.src = `../images/003/AirPods (${1 + i}).jpg`;
+      imgElem3.src = `./images/003/AirPods (${1 + i}).jpg`;
       sceneInfo[3].objs.videoImages.push(imgElem3)
     }
 
     let imgElem4;
     for (let i = 0; i < sceneInfo[0].objs.imgPath.length; i++) {
       imgElem4 = new Image();
-      imgElem4.src = `../images/AirPods (${1 + i}).png`;
+      imgElem4.src = `./images/AirPods (${1 + i}).png`;
       sceneInfo[0].objs.images.push(imgElem4);
     }
   }
@@ -294,8 +301,8 @@
     let scrollRatio = currentYOffset / scrollHeight; //현제 섹션에서 몇퍼센트 만큼 스크롤 했는지 구하는 식
     switch (currentScene) {
       case 0:
-        objs.context.drawImage(objs.images[0], 380, -50);
-        objs.context.drawImage(objs.images[1], 1070, 400);
+        objs.context.drawImage(objs.images[0], 380, -50);  // 0번째 섹션 에어팟 그리기
+        objs.context.drawImage(objs.images[1], 1070, 400); // 0번째 섹션 에어팟 그리기
         objs.canvas.style.transform = `scale(${calcValues(values.canvas_scale, currentYOffset)})`
         
         //messageA
@@ -312,7 +319,6 @@
         }
         break;
       case 1:
-        
         // let sequence = Math.round(calcValues(values.imageSeqeunce,currentYOffset));
         // objs.context.drawImage(objs.videoImages[sequence], 0, 0, objs.canvas.width, objs.canvas.height);
         if (scrollRatio <= 0.5) {
@@ -346,11 +352,9 @@
 
         break;
       case 2:
-        
         // let sequence2 = Math.round(calcValues(values.imageSeqeunce,currentYOffset));
         // objs.context.drawImage(objs.videoImages[sequence2], 0, 0, objs.canvas.width, objs.canvas.height);
-        
-          objs.canvas.style.opacity = calcValues(values.canvas_opacity_out,currentYOffset)
+        objs.canvas.style.opacity = calcValues(values.canvas_opacity_out,currentYOffset)
         //messageA
         if (scrollRatio <= 0.25) {
           objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset);
@@ -372,7 +376,6 @@
         }
         break;
       case 3:
-
         // let sequence3 = Math.round(calcValues(values.imageSeqeunce,currentYOffset));
         // objs.context.drawImage(objs.videoImages[sequence3], 0, 0, objs.canvas.width, objs.canvas.height);
         if (scrollRatio <= 0.5) {
@@ -406,7 +409,6 @@
         if (scrollRatio < 0.6) {
           objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${canvasScaleRatio})`;
         } else if (scrollRatio >= 0.6) {
-          
           values.canvas_scale[1] = window.innerWidth / (objs.canvas.width * 2.1);
           objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${calcValues(values.canvas_scale, currentYOffset)})`
         }
@@ -445,9 +447,11 @@
   let rafId;
   let rafState;
   function loop() {
+    //부드러운 스크롤 효과를 위해 delayYOffSet 값을 계산
     delayYOffSet = delayYOffSet + (yOffset - delayYOffSet) * acc;
     if (!enterNewScene) {
       if (currentScene === 1 || currentScene === 2 || currentScene === 3) {
+        //섹션 1, 2, 3일때만 실행
         const objs = sceneInfo[currentScene].objs;
         const values = sceneInfo[currentScene].values;
         let currentYOffset = delayYOffSet - prevScrollHeight;
@@ -473,12 +477,12 @@
     sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.images[1], 1070, 400);
     setLayout();
 
+    //새로고침할때 마다 살짝 스크롤을 움직이는 식 = 잘못 나온 컨텐츠들을 제대로 돌려놓음
     let tempYOffset = yOffset;
     let tempScrollCount = 0;
     let siId = setInterval(() => {
       window.scrollTo(0, tempYOffset);
       tempYOffset += 2;
-
       if (tempScrollCount > 20) {
         clearInterval(siId);
       }
